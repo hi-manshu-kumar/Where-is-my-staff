396cba7f3d112292e98385df82165b1189ef5aa3
hi-manshu-kumar <himanshu.kumar394@gmail.com>
2018-07-08 19:12:00 +0530

    Added toggle feature in login/register

--- js/controllers/userController.js
+++ js/controllers/userController.js
@@ -1,3 +1,33 @@
 myApp.controller('userCtrl',function($scope,userfactory){
-    $scope.x=false;
+    $scope.isShowHideLog=false;
+    var x =$scope.isShowHideLog;
+    $scope.isShowHideReg=false;
+    var y =$scope.isShowHideReg;
+    $scope.showLogin = function(){
+        console.log(
+            userfactory.showLogin(x,y).isShowHide 
+         , userfactory.showLogin().isShowHideReg
+    );  
+        x=false;
+        y=true;
+         x = userfactory.showLogin(x,y).isShowHide;
+         y = userfactory.showLogin(x,y).isShowHideReg;
+        console.log({x,y});
+        $scope.isShowHideLog = x;
+        $scope.isShowHideReg = y;        
+    };
+
+    $scope.showReg = function(){
+        console.log(
+            userfactory.showLogin(x,y).isShowHide 
+        // , userfactory.showLogin().isShowHideReg
+    );  
+        x = true;
+        y=true;
+        x = userfactory.showReg(x,y).x;
+        y = userfactory.showReg(x,y).y;
+        console.log({x,y}); 
+        $scope.isShowHideLog = x; 
+        $scope.isShowHideReg = y;       
+    };
 })
