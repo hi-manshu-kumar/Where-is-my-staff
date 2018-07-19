c1dcc48468d0b74bc7d51a9f982523827fe75dea
hi-manshu-kumar <himanshu.kumar394@gmail.com>
2018-07-19 16:50:00 +0530

    integrating leaflet-map

--- yeoman-strucutre/app/scripts/controllers/about.js
+++ yeoman-strucutre/app/scripts/controllers/about.js
@@ -124,5 +124,24 @@ angular.module('whereApp')
       $scope.empType = users.$getRecord(a.$id).type;
   }
 
+  angular.extend($scope, {
+        osloCenter: {
+            lat: 59.91,
+            lng: 10.75,
+            zoom: 12
+        },
+        markers: {
+            osloMarker: {
+                lat: 59.91,
+                lng: 10.75,
+                message: "I want to travel here!",
+                focus: true,
+                draggable: false
+            }
+        },
+        defaults: {
+            scrollWheelZoom: false
+        }
+    });
 }]);
   
